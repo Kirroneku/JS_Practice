dc4777a8042060b7dcb017b12c41a48e052b9a8a
Jialin Shan <chansukeizumi@gmail.com>
2020-03-05 16:44:51 -0500

    finished no_repeats

--- freeCodeCamp/Algorithms/no_repeats_please.js
+++ freeCodeCamp/Algorithms/no_repeats_please.js
@@ -7,15 +7,28 @@
 */
 
 function permAlone(str) {
+    return permHelper(str, "");
+}
+  
+function permHelper(str, lastChar) {
+    if( str == "" ) return 1;
     // Make it into a char array
     // Recurse to create perms...
     // If perm has this current and next be the same char, just return 0
     // Once we cannot make more perms return 1 so we can add
     // Remove letter from candidate list once used
     // Return letter to candidate list once we come back to make sure others can use it again
+    
+    let total = 0;
+    for(let i = 0; i < str.length; i++){
+        if( lastChar != str.substring(i, i+1)) {
+            // console.log(lastChar);
+            total += permHelper(str.substring(0, i) + str.substring(i+1), str.substring(i, i+1));
+        }
+    }
 
-    return str;
+    return total;
 }
-  
-permAlone('aab');
+
+console.log(permAlone('abfdefa'));
   
