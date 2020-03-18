b2035f3e4edf68405661f2bdf1bf99b144c9f1f5
Jialin Shan <chansukeizumi@gmail.com>
2020-03-17 21:59:14 -0400

    Finished circular queue

--- freeCodeCamp/data structures/circular_queue.js
+++ freeCodeCamp/data structures/circular_queue.js
@@ -7,8 +7,9 @@ class CircularQueue {
       this.queue = [];
       this.read = 0;
       this.write = 0;
-      this.max = size - 1;
- 
+      this.max = size;
+      this.diff = 0;
+
       while (size > 0) {
          this.queue.push(null);
          size--;
@@ -20,11 +21,33 @@ class CircularQueue {
     }
  
     enqueue(item) {
-
+      if(this.diff == this.max) {
+        return null;
+      }
+      this.diff ++;
+      this.queue[this.write] = item;
+      this.write = (this.write + 1) % this.max;
+      return item;
     }
  
     dequeue() {
-      
+      if(this.diff === 0) {
+        return null;
+      }
+      this.diff--;
+      let readItem = this.queue[this.read];
+      this.queue[this.read] = null;
+
+      this.read = (this.read + 1) % this.max;
+      return readItem;
     }
  }
  
+ let i = new CircularQueue(4);
+ console.log(i.enqueue("a"));
+ console.log(i.enqueue("b"));
+ console.log(i.enqueue("c"));
+ console.log(i.dequeue());
+ console.log(i.dequeue());
+ console.log(i.dequeue());
+ console.log(i.dequeue());
