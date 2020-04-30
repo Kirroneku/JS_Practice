28710aaa5d78a92885b57bb1cf3724a318916df7
Jialin Shan <chansukeizumi@gmail.com>
2020-04-30 19:25:50 -0400

    p12, p13

---
+++ freeCodeCamp/Project Euler/P12_Highly_divisible_triangular_number.js
@@ -0,0 +1,28 @@
+function divisibleTriangleNumber(n) {
+    if( n == 1 ) {
+        return 1;
+    }
+
+    let tri = 2;
+    let number = 3;
+
+    while( true ) {
+        let sqrtofNum = Math.ceil(Math.sqrt(3))
+        let totalFactors = 0;
+        for( let i = 1; i < sqrtofNum; i++ ) {
+            if( number % i == 0 ) {
+                totalFactors += 2;
+            }
+        }
+
+        if( totalFactors >= n ) {
+            return number;
+        }
+
+        tri++;
+        number += tri;
+    }
+}
+  
+divisibleTriangleNumber(500);
+  
